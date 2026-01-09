6938843791b00e73df9c0620d48f68a9d184efc9
DamodaraBarbosa <damodarabarbosa@gmail.com>
2026-01-09 15:10:05 -0300

    feat: adicao de camara client file

---
+++ src/clients/camara_client.py
@@ -0,0 +1,14 @@
+import requests
+
+class CamaraCliente:
+    def __init__(self, url='https://dadosabertos.camara.leg.br/api/v2/'):
+        self.url = url
+
+    def get(self, endpoint: str):
+        if not endpoint:
+            raise ValueError('The endpoint parameter must be not empty.')
+        
+        url = f'{self.url}{endpoint}'
+        request = requests.get(url)
+
+        return request.json()
