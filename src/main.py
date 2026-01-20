e12616b067c5ccc32dec9791490ba3abe080d1a8
DamodaraBarbosa <damodarabarbosa@gmail.com>
2026-01-20 12:38:49 -0300

    feat: added eventos ids py file extractor

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from clients.camara_client import AsyncCamaraClient
 from extractors.camara.orgaos.orgaos import AsyncOrgaosExtractor
+from extractors.camara.orgaos.ids import AsyncOrgaosIdsExtractor
 import asyncio
 from datetime import datetime
 
@@ -11,17 +12,17 @@ async def main():
     orgaos_data = await orgaos.extract(init_legislatura=56)
 
     end = datetime.now()
-    print(f'Total votacoes extracted: {len(orgaos_data)}')
+    print(f'Total orgaos extracted: {len(orgaos_data)}')
     print(f'Time elapsed: {end - start}')
 
-    # votacoes_ids = AsyncVotacoesIdsExtractor(client)
-    # start = datetime.now()
+    orgaos_ids = AsyncOrgaosIdsExtractor(client)
+    start = datetime.now()
 
-    # votacoes_ids_data = await votacoes_ids.extract(votacoes_data)
-    # end = datetime.now()
+    orgaos_ids_data = await orgaos_ids.extract(orgaos_data)
+    end = datetime.now()
 
-    # print(f'Total votacoes ids extracted: {len(votacoes_ids_data)}')
-    # print(f'Time elapsed: {end - start}')
+    print(f'Total orgaos ids extracted: {len(orgaos_ids_data)}')
+    print(f'Time elapsed: {end - start}')
 
 if __name__ == '__main__':
     asyncio.run(main())
