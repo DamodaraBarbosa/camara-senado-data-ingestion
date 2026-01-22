7042c3fdf195bd5331604e48d06acbb7a581faf7
DamodaraBarbosa <damodarabarbosa@gmail.com>
2026-01-21 21:19:13 -0300

    feat: added partidos py file

--- src/main.py
+++ src/main.py
@@ -1,29 +1,17 @@
 from clients.camara_client import AsyncCamaraClient
-from extractors.camara.orgaos.orgaos import AsyncOrgaosExtractor
-from extractors.camara.orgaos.situacoes_orgao import SituacoesOrgaoExtractor
+from extractors.camara.partidos.partidos import AsyncPartidosExtractor
 import asyncio
 from datetime import datetime
 
 async def main():
     client = AsyncCamaraClient()
-    orgaos = AsyncOrgaosExtractor(client)
+    partidos = AsyncPartidosExtractor(client)
     start = datetime.now()
 
-    orgaos_data = await orgaos.extract(init_legislatura=56)
+    partidos_data = await partidos.extract(init_legislatura=56)
 
     end = datetime.now()
-    print(f'Total orgaos extracted: {len(orgaos_data)}')
-    print(f'Time elapsed: {end - start}')
-
-    situacoes_orgao = SituacoesOrgaoExtractor(client)
-
-    start = datetime.now()
-
-    codigo_situacao_data = await situacoes_orgao.extract()
-
-    end = datetime.now()
-
-    print(f'Total codigos situacao: {codigo_situacao_data}')
+    print(f'Partidos extracted: {partidos_data}')
     print(f'Time elapsed: {end - start}')
 
 if __name__ == '__main__':
