7b562e1db81a8d89ea4b83eb62e13a122ea63f3a
DamodaraBarbosa <damodarabarbosa@gmail.com>
2026-01-22 12:00:51 -0300

    feat: added legislaturas py file

--- src/main.py
+++ src/main.py
@@ -1,29 +1,17 @@
 from clients.camara_client import AsyncCamaraClient
-from extractors.camara.partidos.partidos import AsyncPartidosExtractor
-from extractors.camara.partidos.membros import AsyncPartidosMembrosExtractor
+from extractors.camara.legislaturas.legislaturas import AsyncLegislaturaExtractor
 import asyncio
 from datetime import datetime
 
 async def main():
     client = AsyncCamaraClient()
-    partidos = AsyncPartidosExtractor(client)
+    legislaturas = AsyncLegislaturaExtractor(client)
     start = datetime.now()
 
-    partidos_data = await partidos.extract(init_legislatura=56)
+    legislaturas_data = await legislaturas.extract(init_legislatura=56)
 
     end = datetime.now()
-    # print(f'Partidos extracted: {partidos_data}')
-    print(f'Time elapsed: {end - start}')
-
-    membros = AsyncPartidosMembrosExtractor(client)
-    start = datetime.now()
-
-    membros_data = await membros.extract(partidos=partidos_data, init_legislatura=56)
-    # ids = AsyncPartidosIdsExtractor(client)
-    # ids_data = await ids.extract(partidos=partidos_data)
-    
-    end = datetime.now()
-    print(f'Total of membros: {membros_data}')
+    print(legislaturas_data)
     print(f'Time elapsed: {end - start}')
 
 if __name__ == '__main__':
