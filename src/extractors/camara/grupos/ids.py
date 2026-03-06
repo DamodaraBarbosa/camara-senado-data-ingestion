acf8861bc6842dcbe2f2fa5ad3eeafa4906333f0
Damodara Barbosa <damodarabarbosa@gmail.com>
2026-03-06 19:15:15 -0300

    feat: added the grupos ids extractor py file

---
+++ src/extractors/camara/grupos/ids.py
@@ -0,0 +1,28 @@
+from extractors.camara.base import CamaraBaseExtractor
+import aiohttp
+import asyncio
+
+class AsyncGruposIdsExtractor(CamaraBaseExtractor):
+    ENDPOINT = 'grupos/{id}'
+
+    async def extract(
+            self, 
+            grupos
+        ):
+        grupos_ids = list(grupo.get('id') for grupo in grupos if grupo.get('id'))
+        all_ids = []
+
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+
+            for grupo in grupos_ids:
+                task = self.client.get(session, self.ENDPOINT.format(id=grupo))
+                tasks.append(task)
+
+            results = await asyncio.gather(*tasks)
+
+            for result in results:
+                ids_data = result.get('dados', {})
+                all_ids.append(ids_data)
+
+        return all_ids
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from clients.camara_client import AsyncCamaraClient
 from extractors.camara.grupos.grupos import AsyncGruposExtractor
+from extractors.camara.grupos.ids import AsyncGruposIdsExtractor
 import asyncio
 from datetime import datetime
 
@@ -10,11 +11,13 @@ async def main():
     start = datetime.now()
     grupos_data = await grupos.extract(init_legislatura=56)
 
+    grupos_ids_extractor = AsyncGruposIdsExtractor(client)
+    grupos_ids_data = await grupos_ids_extractor.extract(grupos_data)
+
     end = datetime.now()
 
+    print(f'Grupos IDs: {grupos_ids_data}')
     print(f'Tempo gasto: {end - start}')
-    print(f'Len grupos: {len(grupos_data)}')
-    print(f'Grupos: {grupos_data}')
 
 if __name__ == '__main__':
     asyncio.run(main())
