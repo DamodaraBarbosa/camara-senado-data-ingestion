116773ac254be7ac302cd7d0551efacd18e695c6
Damodara Barbosa <damodarabarbosa@gmail.com>
2026-03-06 19:37:23 -0300

    feat: added grupos membros extractor file py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from clients.camara_client import AsyncCamaraClient
 from extractors.camara.grupos.grupos import AsyncGruposExtractor
 from extractors.camara.grupos.ids import AsyncGruposIdsExtractor
 from extractors.camara.grupos.historico import AsyncGruposHistoricoExtractor
+from extractors.camara.grupos.membros import AsyncGruposMembrosExtractor
 import asyncio
 from datetime import datetime
 
@@ -14,12 +15,13 @@ async def main():
 
     grupos_ids_extractor = AsyncGruposIdsExtractor(client)
     grupos_ids_data = await grupos_ids_extractor.extract(grupos_data)
-    grupos_historico_extractor = AsyncGruposHistoricoExtractor(client)
-    grupos_historico_data = await grupos_historico_extractor.extract(grupos_data)
+    grupos_membros_extractor = AsyncGruposMembrosExtractor(client)
+    grupos_membros_data = await grupos_membros_extractor.extract(grupos_data)
+
     end = datetime.now()
 
-    print(f'Grupos IDs: {grupos_historico_data}')
-    print(f'Número de histórico de grupos: {len(grupos_historico_data)}')
+    print(f'Grupos IDs: {grupos_membros_data}')
+    print(f'Número de histórico de grupos: {len(grupos_membros_data)}')
     print(f'Tempo gasto: {end - start}')
 
 if __name__ == '__main__':
