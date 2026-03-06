23f3529b324fe3264c08d405f53342297438e5b7
Damodara Barbosa <damodarabarbosa@gmail.com>
2026-03-06 19:25:50 -0300

    feat: added historico py file for grupos

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from clients.camara_client import AsyncCamaraClient
 from extractors.camara.grupos.grupos import AsyncGruposExtractor
 from extractors.camara.grupos.ids import AsyncGruposIdsExtractor
+from extractors.camara.grupos.historico import AsyncGruposHistoricoExtractor
 import asyncio
 from datetime import datetime
 
@@ -13,11 +14,12 @@ async def main():
 
     grupos_ids_extractor = AsyncGruposIdsExtractor(client)
     grupos_ids_data = await grupos_ids_extractor.extract(grupos_data)
-
+    grupos_historico_extractor = AsyncGruposHistoricoExtractor(client)
+    grupos_historico_data = await grupos_historico_extractor.extract(grupos_data)
     end = datetime.now()
 
-    print(f'Grupos IDs: {grupos_ids_data}')
-    print(f'Número de IDs de grupos: {len(grupos_ids_data)}')
+    print(f'Grupos IDs: {grupos_historico_data}')
+    print(f'Número de histórico de grupos: {len(grupos_historico_data)}')
     print(f'Tempo gasto: {end - start}')
 
 if __name__ == '__main__':
