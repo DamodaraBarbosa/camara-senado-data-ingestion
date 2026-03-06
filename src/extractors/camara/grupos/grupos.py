ebf71891564ea22ca70ab415e6d4c218f964f43c
Damodara Barbosa <damodarabarbosa@gmail.com>
2026-03-06 00:00:12 -0300

    feat: added grupos py file

---
+++ src/extractors/camara/grupos/grupos.py
@@ -0,0 +1,69 @@
+from extractors.camara.base import CamaraBaseExtractor
+import aiohttp
+import asyncio
+
+class AsyncGruposExtractor(CamaraBaseExtractor):
+    ENDPOINT = 'grupos'
+    LEGISLATURAS = 'legislaturas'
+
+    async def _fetch_pages(
+        self,
+        session,
+        id_legislatura,
+        request_tries,
+        itens  
+    ):
+        extracted_data = []
+        page = 1
+        empty_count = 0
+        
+        while empty_count < request_tries:
+            try:
+                current_params = {
+                    'itens': itens,
+                    'pagina': page
+                }
+
+                current_params = {k: v for k, v in current_params.items() if v is not None}
+
+                response = await self.client.get(session, self.ENDPOINT, params=current_params)
+                data = response.get('dados', [])
+
+                if not data:
+                    empty_count += 1
+                    page += 1
+                    continue
+
+                for grupo in data:
+                    grupo['idLegislatura'] = id_legislatura
+                    extracted_data.append(grupo)
+
+                page += 1
+                empty_count = 0
+
+            except Exception as e:
+                print(f"Error fetching data for legislatura {id_legislatura}, page {page}: {e}")
+                empty_count += 1
+        
+        return extracted_data
+
+    async def extract(
+            self,
+            init_legislatura: int = None,
+            itens: int = 100,
+            request_tries: int = 4
+        ):
+        all_grupos = []
+
+        async with aiohttp.ClientSession() as session:
+            legislaturas = await self.client.get(session, self.LEGISLATURAS)
+            legislaturas_data = legislaturas.get('dados', [])
+            current_legislatura = max([l['id'] for l in legislaturas_data]) if legislaturas_data else 0
+
+            start = init_legislatura if init_legislatura is not None else current_legislatura   
+
+            for id_legislatura in range(start, current_legislatura + 1):
+                frentes_data = await self._fetch_pages(session, id_legislatura, request_tries, itens)
+                all_grupos.extend(frentes_data)
+        
+        return all_grupos     
--- src/main.py
+++ src/main.py
@@ -1,23 +1,20 @@
 from clients.camara_client import AsyncCamaraClient
-from extractors.camara.frentes.frentes import AsyncFrentesExtractor
-from extractors.camara.frentes.membros import AsyncFrentesMembrosExtractor
+from extractors.camara.grupos.grupos import AsyncGruposExtractor
 import asyncio
 from datetime import datetime
 
 async def main():
     client = AsyncCamaraClient()
-    frentes = AsyncFrentesExtractor(client)
+    grupos = AsyncGruposExtractor(client)
 
     start = datetime.now()
-    frentes_data = await frentes.extract(init_legislatura=56)
-    frentes_membros = AsyncFrentesMembrosExtractor(client)
-    frentes_membros_data = await frentes_membros.extract(frentes_data)
+    grupos_data = await grupos.extract(init_legislatura=56)
 
     end = datetime.now()
 
     print(f'Tempo gasto: {end - start}')
-    print(f'Len frentes: {len(frentes_membros_data)}')
-    print(f'Frentes: {frentes_membros_data}')
+    print(f'Len grupos: {len(grupos_data)}')
+    print(f'Grupos: {grupos_data}')
 
 if __name__ == '__main__':
     asyncio.run(main())
