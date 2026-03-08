17ac0e98a0dd58ca61bf8ebb049b2bf4c8c8813e
Damodara Barbosa <damodarabarbosa@gmail.com>
2026-03-08 17:04:36 -0300

    feat: adjust main file to test eventos extractors

--- src/main.py
+++ src/main.py
@@ -1,27 +1,19 @@
 from clients.camara_client import AsyncCamaraClient
-from extractors.camara.grupos.grupos import AsyncGruposExtractor
-from extractors.camara.grupos.ids import AsyncGruposIdsExtractor
-from extractors.camara.grupos.historico import AsyncGruposHistoricoExtractor
-from extractors.camara.grupos.membros import AsyncGruposMembrosExtractor
+from extractors.camara.eventos.eventos import AsyncEventosExtractor
 import asyncio
 from datetime import datetime
 
 async def main():
     client = AsyncCamaraClient()
-    grupos = AsyncGruposExtractor(client)
+    eventos = AsyncEventosExtractor(client)
 
     start = datetime.now()
-    grupos_data = await grupos.extract(init_legislatura=56)
-
-    grupos_ids_extractor = AsyncGruposIdsExtractor(client)
-    grupos_ids_data = await grupos_ids_extractor.extract(grupos_data)
-    grupos_membros_extractor = AsyncGruposMembrosExtractor(client)
-    grupos_membros_data = await grupos_membros_extractor.extract(grupos_data)
+    eventos_data = await eventos.extract(init_legislatura=56)
 
     end = datetime.now()
 
-    print(f'Grupos IDs: {grupos_membros_data}')
-    print(f'Número de histórico de grupos: {len(grupos_membros_data)}')
+    print(f'Eventos: {eventos_data}')
+    print(f'Número de eventos: {len(eventos_data)}')
     print(f'Tempo gasto: {end - start}')
 
 if __name__ == '__main__':
