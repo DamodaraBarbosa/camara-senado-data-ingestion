e12f7074bcebcc10dc5d4a1263b6134e04c3385d
Damodara Barbosa <damodarabarbosa@gmail.com>
2026-05-24 20:11:49 -0300

    feat: improved syc preposicoes file

--- src/main.py
+++ src/main.py
@@ -1,23 +1,19 @@
 from clients.camara_client import AsyncCamaraClient
-from extractors.camara.deputados.deputados import AsyncDeputadosExtractor
-from extractors.camara.deputados.orgaos import AsyncOrgaosExtractor
+from extractors.camara.proposicoes.proposicoes import AsyncProposicoesExtractor
 import asyncio
 from datetime import datetime
 
 
 async def main():
     client = AsyncCamaraClient()
-    deputados = AsyncDeputadosExtractor(client)
+    proposicoes = AsyncProposicoesExtractor(client)
 
     start = datetime.now()
-    deputados_data = await deputados.extract(init_legislatura=56)
-
-    orgao = AsyncOrgaosExtractor(client)
-    orgaos_data = await orgao.extract(deputados_data, init_legislatura=56)
+    proposicoes_data = await proposicoes.extract(init_legislatura=56)
 
     end = datetime.now()
 
-    print(f'Número de orgãos: {len(orgaos_data)}')
+    print(f'Número de proposições: {len(proposicoes_data)}')
     print(f'Tempo gasto: {end - start}')
 
 if __name__ == '__main__':
